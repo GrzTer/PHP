e55e9225526ffe800151cb46d19ba02b5123dfb9
DevGreg <grzegorz.tereszkiewicz@infotech.edu.pl>
2023-11-03 12:08:41 +0100

    .

--- Lekcja2/ProcessRegister.php
+++ Lekcja2/ProcessRegister.php
@@ -1,22 +1,12 @@
 <?php
 
-if (empty($_POST["name"])) {
-    die("Name is required");
-}
-
-if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    die("Valid email is required");
-}
-
-
-
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__. "/database.php";
 
 $sql = "INSERT INTO user (name, email, password_hash) VALUES (?, ?, ?)";
 
-$stmt = $mysqli->stmt_init();
+$stmt = stmt_init();
 
 if ( ! $stmt->prepare($sql)) {
     die("Sql failed: ". $mysqli->error);
