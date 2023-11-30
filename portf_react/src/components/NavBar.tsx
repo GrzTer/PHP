2f77c97d4f1caf2526a465022f4a463dc76b3134
DevGreg <grzegorz.tereszkiewicz@infotech.edu.pl>
2023-11-30 10:46:11 +0100

    ...

--- portf_react/src/components/NavBar.tsx
+++ portf_react/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './NavBar.css';
+import { Button } from 'react-bootstrap';
 
 interface NavBarProps {
   isAuthenticated: boolean;
@@ -26,16 +27,16 @@ const NavBar: React.FC<NavBarProps> = ({ isAuthenticated, onSignOut }) => {
     <nav className={`NavBar ${isNavBarFixed ? 'fixed' : ''}`}>
       <div className="logo">Portfolio</div>
       <ul className="nav-links">
-        <a href="/#home"><li>Home</li></a>
-        <a href="/#about"><li>About</li></a>
-        <a href="/#projects"><li>Projects</li></a>
-        <a href="/#contact"><li>Contact</li></a>
+        <a href="/#home"><li><Button>Home</Button></li></a>
+        <a href="/#about"><li><Button>About</Button></li></a>
+        <a href="/#projects"><li><Button>Projects</Button></li></a>
+        <a href="/#contact"><li><Button>Contact</Button></li></a>
         {isAuthenticated ? (
           <li onClick={onSignOut}>Sign Out</li>
         ) : (
           <>
-            <li onClick={() => console.log('Sign In')}>Sign In</li>
-            <a href="/signup"><li onClick={() => console.log('Sign Up')}>Sign Up</li></a>
+            <li onClick={() => console.log('Sign In')}><Button>Sign In</Button></li>
+            <a href="/signup"><li onClick={() => console.log('Sign Up')}><Button>Sign Up</Button></li></a>
           </>
         )}
       </ul>
