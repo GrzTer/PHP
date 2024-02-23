e90ea1c0034dae0b27cbc6647b7892abc5077af1
DevGreg <grzegorz.tereszkiewicz@infotech.edu.pl>
2024-02-23 08:51:22 +0100

    Revert "Merge"
    This reverts commit 1adc6336cd7c42fef69e6bd93661b3210d659dcc.

--- Lekcja2/Home.php
+++ Lekcja2/Home.php
@@ -1,75 +1,90 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=google">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/main.css">
-    <link rel="icon" href="styles/favico.ico" />
+    <link rel="icon" href="styles/favico.ico">
     <title>Strona Główna</title>
 </head>
-
 <body>
-    <header>
+<header>
         <div class="header_container">
-            <h4>Strona Główna</h4>
-            <nav>
-                <ul>
-                    <li><a href="Home.php">Strona Główna</a></li>
-                    <li><a href="Onas.php">Projekty</a></li>
-                    <li><a href="Kontakt.php">Kontakt</a></li>
-                    <li><a href="Login.php">Zaloguj się</a></li>
+            <h4>Grzegorz Tereszkiewicz</h4>
+            <ul>
+                <li><a href="home.php">Strona Główna</a></li>
+                <li><a href="shop.php">Sklep</a></li>
+                <li><a href="#about-me">O Mnie</a></li>
+                <li><a href="#projects">Projekty</a></li>
+                <li><a href="#contact-me">Kontakt</a></li>
+                <?php if (isset($_SESSION["user_name"])): ?>
+                    <li><a href="logout.php">Wyloguj się</a></li>
+                <?php else: ?>
+                    <li><a href="login.php">Zaloguj się</a></li>
                     <li><a href="RegisterPage.php">Zarejestruj się</a></li>
-                </ul>
-            </nav>
+                <?php endif; ?>
+            </ul>
         </div>
     </header>
+
+    <div class="underheader">
+        <h1>Witaj na Moim Portfolio</h1>
+        <h2><?php if (isset($_SESSION["user_name"]))
+            echo "Witaj, " . $_SESSION["user_name"]; ?></h2>
+        <p>Tutaj znajdziesz informacje o mnie i moich projektach.</p>
+    </div>
+
     <section id="about-me">
-      <h2>About Me</h2>
-      <p>Hi there, I'm Gregory Tereszkiewicz with over [Number of Years] years of experience in the field.<br><br>
-          I received my [Degree] from [University/College] and have since gone on to achieve a number of professional accomplishments, including [Professional Accomplishment 1], [Professional Accomplishment 2], and [Professional Accomplishment 3].<br><br>
-          My areas of expertise include [Expertise 1], [Expertise 2], and [Expertise 3]. Outside of work, I enjoy [Hobby/Interest 1], [Hobby/Interest 2], and [Hobby/Interest 3]. I am motivated by [What Motivates You] and am always striving to [What You Are Striving To Do].<br><br>
-          What sets me apart from others in my field is [What Sets You Apart]. For anyone starting out in this industry, my advice would be [Your Advice]. Looking ahead, my future goals and aspirations include [Future Goal 1], [Future Goal 2], and [Future Goal 3].<br><br>
-          I'm excited to continue growing and learning in my field and am always looking for new challenges and opportunities to make a positive impact.</p>
-    <img src="{{ url_for('static', filename='[removal.ai]_tmp-64198bbda4618.png') }}"/></section>
+        <h2>O Mnie</h2>
+        <p>scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae 
+            nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem 
+            sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere 
+            sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque 
+            id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi 
+            tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis 
+            hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus 
+            et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit</p>
+        <!-- <img src="./styles/[removal.ai]_tmp-64198bbda4618.png"/> -->
+    </section>
 
     <section id="projects">
-      <h2>Projects</h2>
-      <ul>
-        <li>
-          <h3>Project 1</h3>
-          <p>Description of project 1.</p>
-          <a href="#">Link to project 1</a>
-        </li>
-        <li>
-          <h3>Project 2</h3>
-          <p>Description of project 2.</p>
-          <a href="#">Link to project 2</a>
-        </li>
-      </ul>
+        <h2>Projekty</h2>
+        <ul>
+            <li>
+                <h3>Projekt 1</h3>
+                <a href="#">Link do projektu 1</a>
+                <p>Opis projektu 1.</p>
+            </li>
+            <li>
+                <h3>Projekt 2</h3>
+                <a href="#">Link do projektu 2</a>
+                <p>Opis projektu 2.</p>
+            </li>
+        </ul>
     </section>
 
     <section id="contact-me">
-      <h2>Contact Me</h2>
-      <form action="{{ url_for('contact') }}" method="POST">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" required>
+        <h2>Kontakt</h2>
+        <form action="{{ url_for('contact') }}" method="POST">
+            <label for="name">Imię</label>
+            <input type="text" id="name" name="name" required>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
 
-        <label for="message">Message</label>
-        <textarea id="message" name="message" required></textarea>
+            <label for="message">Wiadomość</label>
+            <textarea id="message" name="message" required></textarea>
 
-        <button type="submit">Send Message</button>
-      </form>
+            <button type="submit">Wyślij Wiadomość</button>
+        </form>
     </section>
+
     <footer>
-        <div class="container">
-            <p>&copy; 2023 Grzegorz Tereszkiewicz</p>
-        </div>
+        <p>Prawa autorskie © Grzegorz Tereszkiewicz</p>
     </footer>
 </body>
-
 </html>
